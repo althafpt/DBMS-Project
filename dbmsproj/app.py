0114b3a3fa997de407c536d6b8ba459c9b359e17
AaronJ17 <aaronjohn2212@gmail.com>
2023-01-09 22:17:15 +0530

    Update app.py

--- dbmsproj/app.py
+++ dbmsproj/app.py
@@ -1,13 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
 @app.route('/cart')
 def cart():
     return render_template('cart.html')
+
+
 @app.route('/login')
 def login():
     return render_template('Login.html')
+
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
