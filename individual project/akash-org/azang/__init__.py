018a16d93c2e4944af8db1faa15b7e907b1804e9
akkaaashhhh <akash.ajithk@gmail.com>
2023-02-07 23:33:11 +0530

    final draft

--- individual project/akash-org/azang/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,render_template,current_app,url_for,redirect,request
-from flask_sqlalchemy import SQLAlchemy
-app = Flask( __name__ )
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///azang.db'
-app.app_context().push()
-db = SQLAlchemy(app)
-
-from azang import routes
